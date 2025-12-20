c05c4b3acfcd25954b39b78d375411d096f3cad6
HanZheng912 <zhenghan912@gmail.com>
2025-12-19 22:55:56 -0500

    integration

--- Final Project Code/backend/src/main/java/com/cs370/places/PlaceSuggestionServlet.java
+++ Final Project Code/backend/src/main/java/com/cs370/places/PlaceSuggestionServlet.java
@@ -14,12 +14,22 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
 public class CORSFilter implements Filter {
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "https://cs-370-final-project-esdtkpveo-hanzheng912s-projects.vercel.app");
         response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+        if (req instanceof HttpServletRequest && "OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
         chain.doFilter(req, res);
     }
     public void init(FilterConfig filterConfig) {}
