71b7668857a2428def913d2821ecae6b8d4ec152
HanZheng912 <zhenghan912@gmail.com>
2025-12-19 23:25:45 -0500

    Update CorsFilter.java

--- Final Project Code/backend/src/main/java/com/cs370/places/CorsFilter.java
+++ Final Project Code/backend/src/main/java/com/cs370/places/CorsFilter.java
@@ -8,21 +8,38 @@ import java.io.IOException;
 
 @WebFilter("/*")
 public class CORSFilter implements Filter {
+
+    private static final String ALLOWED_ORIGIN = "https://cs-370-final-project.vercel.app";
+
+    @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "https://cs-370-final-project-esdtkpveo-hanzheng912s-projects.vercel.app");
-        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-        if (req instanceof HttpServletRequest && "OPTIONS".equalsIgnoreCase(((HttpServletRequest) req).getMethod())) {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        String origin = request.getHeader("Origin");
+
+        if (ALLOWED_ORIGIN.equals(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
             return;
         }
 
         chain.doFilter(req, res);
     }
-    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
     public void destroy() {}
 }
-//new code
+
